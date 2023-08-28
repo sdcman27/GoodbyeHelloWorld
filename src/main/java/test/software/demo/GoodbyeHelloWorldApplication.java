6bfe87319842dfe52263733ed7893da0d31d257f
Seth Chritzman <'>
2023-08-28 16:03:34 -0400

    First commit

---
+++ src/main/java/test/software/demo/GoodbyeHelloWorldApplication.java
@@ -0,0 +1,14 @@
+package test.software.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodbyeHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodbyeHelloWorldApplication.class, args);
+		System.out.println("GoodbyeHelloWorld");
+	}
+
+}
