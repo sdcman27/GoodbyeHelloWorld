403db694d849b0ba619849e05d7b19a32362eb71
oleks <oleks@DESKTOP-22DEFV8.lan>
2023-08-28 16:26:18 -0400

    Test Commit!

--- src/main/java/test/software/demo/GoodbyeHelloWorldApplication.java
+++ src/main/java/test/software/demo/GoodbyeHelloWorldApplication.java
@@ -9,7 +9,7 @@ public class GoodbyeHelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GoodbyeHelloWorldApplication.class, args);
 		System.out.println("GoodbyeHelloWorld");
-		System.out.println("Testing 123!!");
+		System.out.println("Testing 123!!!");
 		
 	}
 
